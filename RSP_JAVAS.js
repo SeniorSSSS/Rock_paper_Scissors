fc778762f0682f503832dfea8323951511aaf64b
SeniorSSSS <sakariye.sed@outlook.com>
2022-05-21 05:48:44 -0400

    Add html,css,java file & Make function computerplay

---
+++ RSP_JAVAS.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+    var number = Math.floor(Math.random() * 4);
+    if (number == 1) {
+        alert ("Rock")
+    }
+    else if (number == 2) {
+        alert ("Paper")
+    }
+    else {
+        alert ("Scissors")
+    }
+  }
